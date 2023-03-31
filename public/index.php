38e80b93055d21f79be98f5ccdc2b4c1b15b1019
Goliath33 <gauliath@msn.com>
2023-03-31 02:28:43 +0200

    test

--- public/index.php
+++ public/index.php
@@ -1,9 +1,53 @@
-<?php
-
-use App\Kernel;
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Balade au fil de l'eau</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+		<header>
+		<div class="ocean">
+  <div class="wave"></div>
+  <div class="wave"></div>
+	  <div class="wave"></div>
+</div>
+    	</header>
+<ul class="topnav">
+			  	<li><a class="active" href="index.html">Accueil</a></li>
+  			-	<li><a href="apropos.html">A propos</a></li>
+  				<li><a href="nosbalades.html">Nos balades</a></li>
+				<li><a href="#equipage">Équipage</a></li>
+  				<li class="right"><a href="contact.html">Contact et Réservation</a></li>
+		</ul>
+<main>
+	</main>
+    <footer class="footer">
+    <ul class="social-icon">
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-facebook"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-twitter"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-linkedin"></ion-icon>
+        </a></li>
+      <li class="social-icon__item"><a class="social-icon__link" href="#">
+          <ion-icon name="logo-instagram"></ion-icon>
+        </a></li>
+    </ul>
+    <ul class="menu">
+      <li class="menu__item"><a class="menu__link" href="#">Accueil</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">A propos</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Nos balades</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Équipage</a></li>
+      <li class="menu__item"><a class="menu__link" href="#">Contact</a></li>
+    </ul>
+    <p>&copy;2022 Balade au fil de l'eau | Tout droit réservé</p>
+  		</footer>
+			<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  			<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
